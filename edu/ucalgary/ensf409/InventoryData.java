dfbd1f5d6f80687a828f4f75c7a46209ef1b69be
s-poon <stevenpoon12.03@gmail.com>
2022-03-30 00:21:56 -0600

    added sql stuff

--- edu/ucalgary/ensf409/InventoryData.java
+++ edu/ucalgary/ensf409/InventoryData.java
@@ -2,13 +2,22 @@ package edu.ucalgary.ensf409;
 
 public class InventoryData {
     // Member Variables
+    private ArrayList<Food> stock;
+    private Client adultMale;
+    private Client adultFemale;
+    private Client childOver8;
+    private Client childUnder8;
 
     // Constructors
-
-    // Setters
-
-    // Getters
-
-    // Methods
     
+    // Setters
+    public void setClientStats(){}
+    // Getters
+    public void getClientStats(){}
+    // Methods
+    public void moveItem(){}
+    public void sortInventory(){}
+    public void search(){}
+    
+
 }
