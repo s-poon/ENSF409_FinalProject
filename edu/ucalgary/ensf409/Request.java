83fd2ddf95fbd99a438623198f61990a35aa314f
s-poon <stevenpoon12.03@gmail.com>
2022-04-18 00:27:50 -0600

    Everything is done and commented

--- edu/ucalgary/ensf409/Request.java
+++ edu/ucalgary/ensf409/Request.java
@@ -12,7 +12,7 @@
 package edu.ucalgary.ensf409;
 
 /**
- * 
+ * Request  
  * 
  * @author Tammy Pham, Steven Poon, Bill Thai and Alex Yeap
  * @version 3.4
@@ -30,6 +30,15 @@ public class Request extends InventoryData{
     private Hamper[] hampers;
 
 /***************************** Constructor ************************************/
+    /**
+     * Sets the variables and initializes the arrays
+     * 
+     * @param numAMIn       The number of adult males for each hamper
+     * @param numAFIn       The number of adult females for each hamper
+     * @param numCO8In      The number of childern over 8 for each hamper
+     * @param numCU8In      The number of childern under 8 for each hamper
+     * @param numHampers    The number of hampers to be made
+     */
     Request(
         int numAMIn[], 
         int numAFIn[], 
@@ -47,22 +56,51 @@ public class Request extends InventoryData{
     }
 
 /******************************* Getters **************************************/
+    /**
+     * @return  The number of hampers in the order
+     */
     public int getNumHampers(){ return this.numHampers; }
+
+    /**
+     * @param i     Which hamper in the order
+     * @return      The number of adult males in the order at i
+     */
     public static int getNumAM(int i){ return numAM[i]; }
+
+    /**
+     * @param i     Which hamper in the order
+     * @return      The number of adult females in the order at i
+     */
     public static int getNumAF(int i){ return numAF[i]; }
+
+    /**
+     * @param i     Which hamper in the order
+     * @return      The number of childern under 8 in the order at i
+     */
     public static int getNumCU(int i){ return numCU8[i]; }
+
+    /**
+     * @param i     Which hamper in the order
+     * @return      The number of childern over 8 in the order at i
+     */
     public static int getNumCO(int i){ return numCO8[i]; }
     
 /****************************** Methods ***************************************/
     /**
-     * 
+     * Takes the information about the order and creates the client list for 
+     * each hamper.  It then calls upon the 
      *        
      * @return      returns 0 if filling the hamper and creating the file was 
      *              sucessful, returns 1 otherwise
      */
     public int fillHampers(){
         for(int i = 0; i < this.numHampers; i ++){
-            this.clientList[i] = new Client[numAF[i] + numAM[i] + numCO8[i] + numCU8[i]];
+            this.clientList[i] = new Client[
+                numAF[i] 
+                + numAM[i] 
+                + numCO8[i]
+                + numCU8[i]
+            ];
             createClientList(i);
             hampers[i] = findPossibleHampers(clientList[i]);
             if(hampers[i] == null){
@@ -75,10 +113,12 @@ public class Request extends InventoryData{
         getDataBase().removeItems(getUsedFood());
         return 0;
     }
-
     
     /** 
-     * @param k     
+     * Adds the number of each client type to the clientList at the specified
+     * index
+     * 
+     * @param k     The index of which hamper is to be filled   
      */
     public void createClientList(int k){
         int j = 0;
